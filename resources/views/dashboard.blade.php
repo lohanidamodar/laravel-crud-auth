9abe3980f981d21c8594e956563754b1bf1ed424
Damodar Lohani <dlohani48@gmail.com>
2018-05-19 13:18:56 +0545

    modifying layout with container in app layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,34 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                @endif
 
-                    @if(isset($listings))
-                        <div class="list-group">
-                            @foreach($listings as $listing)
-                                <div class="list-group-item">
-                                    <h3>
-                                        {{$listing->name}}
-                                    </h3>
-                                    <p>
-                                        {{$listing->address}}
-                                    </p>
-                                </div>
-                            @endforeach
-                        </div>
-                    @endif
-                </div>
+                @if(isset($listings))
+                    <div class="list-group">
+                        @foreach($listings as $listing)
+                            <div class="list-group-item">
+                                <h3>
+                                    {{$listing->name}}
+                                </h3>
+                                <p>
+                                    {{$listing->address}}
+                                </p>
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
             </div>
         </div>
     </div>
